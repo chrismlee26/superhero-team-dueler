9a4e52ea712aa24577d3df05a789d830dad8d996
CHRIS_LEE <chrismlee87@gmail.com>
2021-02-15 02:20:07 -0800

    defend yourself

---
+++ README.md
@@ -0,0 +1,16 @@
+## SUPERHERO TEAM DUELER PROJECT!
+
+![GitHub top language](https://img.shields.io/github/languages/top/chrismlee26/superhero-team-dueler)
+![GitHub last commit](https://img.shields.io/github/last-commit/chrismlee26/superhero-team-dueler)
+
+This project is my Superhero Team Dueler in Python3. This project encompasses the OOP methods: Scope, Objects, Attributes, Methods, Inheritance and Polymorphism. Assignment #2 for CS1.1 at Make School (Object Oriented Programming)
+
+v1.0.0
+
+## Run
+
+$ python3 hero.py to run
+
+$ Or just press the run button in VSCode
+
+## Operation
--- hero.py
+++ hero.py
@@ -1,4 +1,6 @@
 import random
+from ability import Ability
+from armor import Armor
 
 
 class Hero:
@@ -6,15 +8,66 @@ class Hero:
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
+        self.abilities = list()
+        self.armors = list()
+        self.current_health = starting_health
+
+    def add_ability(self, ability):
+        self.abilities.append(ability)
+
+    def attack(self):
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+            return total_damage
+
+    def add_armor(self, armor):
+        # Hasn't been tested yet
+        self.armors.append(armor)
+
+    def defend(self):
+        total_block = 0
+        # We're here right now
+        for ability in self.abilities:
+            total_damage -= ability.defend()
+            return total_block
+
+    def take_damage(self):
+        pass
+
+    def is_alive(self):
+        pass
 
     def fight(self, opponent):
         self.opponent = opponent
         duel = [opponent.name, self.name]
         print("The winner is: ", random.choice(duel))
 
+# Fight Test
+# if __name__ == "__main__":
+#     hero1 = Hero("Wonder Woman")
+#     hero2 = Hero("Dumbledore")
+#     hero1.fight(hero2)
 
+# Override default current_health Test
+# if __name__ == "__main__":
+#     my_hero = Hero("Grace Hopper", 200)
+#     print(my_hero.name)
+#     print(my_hero.current_health)
+
+# Add ability to list test
+# if __name__ == "__main__":
+#     ability = Ability("Great Debugging", 50)
+#     hero = Hero("Grace Hopper", 200)
+#     hero.add_ability(ability)
+#     print(hero.abilities)
+
+
+# Attack Test for abilities
 if __name__ == "__main__":
-    hero1 = Hero("Wonder Woman")
-    hero2 = Hero("Dumbledore")
-
-    hero1.fight(hero2)
+    ability = Ability("Great Debugging", 50)
+    another_ability = Ability("Smarty Pants", 90)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    hero.add_ability(another_ability)
+    print(hero.attack())
