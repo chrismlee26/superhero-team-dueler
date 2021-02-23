2812d39c3d537a0be106d3501171eacea9e3a9b7
CHRIS_LEE <chrismlee87@gmail.com>
2021-02-23 01:27:01 -0800

    pytest beginning

---
+++ team.py
@@ -0,0 +1,27 @@
+class Team:
+    def __init__(self, name):
+        self.name = name
+        self.heroes = list()
+
+    def add_hero(self, hero):
+        self.heroes.append(hero)
+
+    def remove_hero(self, name):
+        foundHero = False
+        # loop through each hero in our list
+        for hero in self.heroes:
+            # if we find them, remove them from the list
+            if hero.name == name:
+                self.heroes.remove(hero)
+                # set our indicator to True
+                foundHero = True
+        # if we looped through our list and did not find our hero,
+        # the indicator would have never changed, so return 0
+        if not foundHero:
+            return 0
+
+    def view_all_heroes(self):
+        '''Prints out all heroes to the console.'''
+        # Loop over the list of heroes and print their names to the terminal one by one.
+        for hero in self.heroes:
+            print(hero)
